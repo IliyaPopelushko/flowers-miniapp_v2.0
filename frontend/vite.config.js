e449462a4e84cc4f2279e4865a7af723e7782565
IliyaPopelushko <108747542+IliyaPopelushko@users.noreply.github.com>
2026-01-13 10:13:29 +0500

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/flowers-miniapp/',
+  base: '/flowers-miniapp_v2.0/',
   server: {
     port: 3000
   },
