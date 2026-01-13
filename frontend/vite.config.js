173074b3c197dbedc0370b7ba5a3558cfa3df7b4
IliyaPopelushko <108747542+IliyaPopelushko@users.noreply.github.com>
2026-01-13 10:04:49 +0500

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,15 +3,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/flowers-miniapp/' : '/',
+  base: '/flowers-miniapp/',
   server: {
     port: 3000
   },
   build: {
     outDir: 'dist',
     assetsDir: 'assets'
-  },
-  define: {
-    'process.env.VITE_API_URL': JSON.stringify(process.env.VITE_API_URL)
   }
 })
